dcc2d0e1be5e43be5f4f512450bf98a52b34a1d4
Doug George <douglas.george@libertymutual.com>
2017-12-22 12:47:41 -0500

    one component version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import {JeopardyService } from './jeopardy.service';
 
 @Component({
@@ -7,9 +7,13 @@ import {JeopardyService } from './jeopardy.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = 'Jeapordy Lite';
 
   questionInfo;
+  userResponse: string;
+  playerTotalScore = 0;
+
+  
 
   constructor(private jeopardyService: JeopardyService){}
 
@@ -26,4 +30,24 @@ export class AppComponent implements OnInit {
     this.getDataFromService()
   }
 
+
+  buttonClicked() {
+    if (this.userResponse == this.questionInfo.answer){
+      this.playerTotalScore = this.playerTotalScore + this.questionInfo.value
+  } else if(this.userResponse != this.questionInfo.answer){
+    this.playerTotalScore = this.playerTotalScore - this.questionInfo.value
+  }
+
+  }
+
+  getNextQuestion() {
+    this.jeopardyService.getQuestionInfo()
+      .subscribe(
+          questionInfo => {
+              this.questionInfo = questionInfo[0];
+          }
+      )
+            this.userResponse = "";
+      }
 }
+
